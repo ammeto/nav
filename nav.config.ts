61468d19aef9e6efb6ad186e96a3ea6b768e3fdb
ammeto <lambammeto@gmail.com>
2021-07-10 13:41:01 +0800

    update: nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,10 +2,10 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/ammeto/nav',
 
   // 部署分支
-  branch: 'main',
+  branch: 'gh-pages',
 
   // 路由是否Hash模式, 如果是部署在github pages 务必设为 true
   hashMode: true,
@@ -14,7 +14,7 @@ const c: IConfig = {
   showGithub: true,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  // homeUrl: 'https://nav3.cn',
 
   // 网站标题
   title: '发现导航 - 精选实用导航网站',
@@ -26,7 +26,7 @@ const c: IConfig = {
   keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
 
   // 默认主题: Light | Sim | Side | App | Shortcut
-  theme: 'Light',
+  theme: 'Sim',
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
@@ -40,7 +40,7 @@ const c: IConfig = {
 
   // 百度统计地址
   // https://tongji.baidu.com/web/welcome/login
-  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?4582be7af7e7c95ef75351e07c6c32ba',
+  // baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?4582be7af7e7c95ef75351e07c6c32ba',
 
   // CNZZ 统计地址
   // https://www.cnzz.com/o_index.php
